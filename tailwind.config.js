41f5592863ceb7aeee422ba3355706c038c1526c
LRamos <luisoares1991@gmail.com>
2025-11-20 18:07:36 -0300

    Correção no uso de tema claro/escuro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Essa linha mágica habilita o botão de tema funcionar
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
